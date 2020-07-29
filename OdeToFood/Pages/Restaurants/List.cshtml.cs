8bc8980da2f6668e3aa6fdb52af39b3cc92f5028
Richard Orchard <me@richardorchard.com>
2020-07-29 12:39:15 +0100

    npm package - adding main project, etc

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+
+   //     private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+
+        public string Message { get; set; }
+
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+
+            this.restaurantData = restaurantData;
+
+
+        }
+
+
+        public void OnGet()
+        {
+            Message = "Hello, World!";
+
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+
+        }
+    }
+}
